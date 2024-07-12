71b0d3fd5e1417d7a2c60c1ec786bd3444b99a2f
João Gouveia <joao.c.gouveia10@gmail.com>
2024-07-12 17:17:50 +0100

    Update AIController.cs

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -9,17 +9,6 @@ public class AIController : MonoBehaviour
     private float health;
     private bool _hasMovedAlready = false;
     private bool _hasAttackedAlready = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void TakeDamage(float damage)
     {
