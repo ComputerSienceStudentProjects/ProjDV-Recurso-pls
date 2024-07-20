91c1fd7d1f322a1a5cd5a8f91cb86d911bb6847b
João Gouveia <joao.c.gouveia10@gmail.com>
2024-07-20 14:21:34 +0100

    fixed

---
+++ Assets/Scripts/DemoEnd.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DemoEnd : MonoBehaviour
+{
+
+    [SerializeField] private UIDocument _uiDocument;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _uiDocument.rootVisualElement.Q<Button>().clicked += Exit;
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit(0);
+    }
+    
+}
